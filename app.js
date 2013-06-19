54cbf9518a291061ad257c5b26488c7d8c438319
Kimbro Staken <kstaken@kstaken.com>
2013-06-19 11:14:57 -0700

    Switching to bower for client dependencies and adding env port setting

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var server = require('http').createServer(app);
 // Hook Socket.io into Express
 var io = require('socket.io').listen(server);
 
+var port = process.env.PORT || 8080;
+
 // Configuration
 
 app.configure(function(){
@@ -21,6 +23,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.static(__dirname + '/public'));
+  app.use(express.static(__dirname + '/components'));  
   app.use(app.router);
 });
 
@@ -46,6 +49,6 @@ io.sockets.on('connection', socket);
 
 // Start server
 
-server.listen(3000, function(){
+server.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
